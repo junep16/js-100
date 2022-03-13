da56db525556c8659778cf9991cbfa930d934d0a
junep16 <jiyunpark0816@gmail.com>
2022-03-13 21:44:14 +0900

    ✨20번 문제

--- 20-20.js
+++ 20-20.js
@@ -1,4 +1,16 @@
 // 문제 19
+// a의 b승 구하기 (제곱)
 
 const num = ["2", "6"]; 
 console.log(Math.pow(num[0], num[1])); 
+
+
+// 문제 20: 몫과 나머지
+// 두 번째 숫자로 첫번째 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력하기. 
+
+let data = prompt().split(" "); 
+// prompt는 값을 받아오는 알러트창같은거! 
+let newData = data[0]/data[1];  
+let 몫 = parseInt(newData); 
+let 나머지 = data[0]%data[1];
+console.log(몫, 나머지); // 2 2 출력!! 
