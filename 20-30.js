a386c94cd5e66862b4dd9662a09bcb8bf8e41d6a
junep16 <jiyunpark0816@gmail.com>
2022-03-15 14:28:40 +0900

    ✅ 26번 문제

--- 20-30.js
+++ 20-30.js
@@ -66,6 +66,34 @@ function findCircleWidth() {
 }
 findCircleWidth()
 
+// 문제 26: 행성 문제
+//행성의 한글 이름을 입력하면 영어 이름을 반환하는 프로그램을 만들기
+
+let planets = {
+    Mercury: "수성",
+    Venus: "금성",
+    Earth: "지구", 
+    Marse: "화성", 
+    Jupiter: "목성", 
+    Saturn: "토성", 
+    Uranus: "천왕성", 
+    Neptune: "해왕성" 
+}
+
+function sayPlanetName() { 
+    let name = prompt(); 
+
+    for (let planet in planets) {
+        if(name === planet) {
+            console.log(`${planets[planet]}`); 
+        } else {
+            console.log("행성 이름이 아닙니다")
+        }
+    }
+}
+sayPlanetName(); 
+
+
 
 
 
