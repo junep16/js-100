096c6ff9873b3552bc17f893b023c6ef28e21dd7
junep16 <jiyunpark0816@gmail.com>
2022-04-08 22:56:52 +0900

    ✅ 37, 38 번 문제

--- 20-40.js
+++ 20-40.js
@@ -211,3 +211,62 @@ const c = one(4);
 console.log(a(10)); 
 console.log(b(10));
 console.log(b(10));
+
+// 문제 36: 구구단 출력하기 
+// 1~9까지의 숫자 중 하나를 입력하면 그 단의 구구단 결과를 한 줄에 출력하는 프로그램 작성하기 
+
+let randomNum = prompt(); 
+for (let i = 0; i<=9; i++) {
+    console.log(`${randomNum} X ${i} = ${randomNum*i}`); 
+}
+
+// 한줄에 출력하기 
+let arr = []; 
+for (let i = 0; i<=9; i++) {
+    arr.push(randomNum*i); 
+}
+console.log(arr); 
+
+
+// 문제 37: 반장 선거
+let student = ['원범', '혜원', '혜원', '혜원', '원범', '혜원', '유진', '지윤', '지윤']; 
+
+function countVote(vote){
+let counts = {}
+
+for(let i =0; i < student.length; i++){ 
+    // 만약 이름이 있으면, key와 value로 넣어주기 (이름이 연속적으로 뜰때까지)
+    if (counts[vote[i]]){
+    counts[vote[i]] += 1 
+    } 
+    // 만약 해당 이름이 아니면 1더해주기
+    else {
+    counts[vote[i]] = 1
+    }
+    }  
+
+    // counts의 키 밸류가 1보다 크거나 같을 경우 다음의 문장을 출력
+    for (let prop in counts){
+        if (counts[prop] >= 0){
+            console.log(prop + "이는" + counts[prop] + " 표를 받았습니다")
+        }
+    }
+    console.log(counts)
+}
+countVote(student); 
+
+// 문제 38: 아르바이트
+let scores = [97, 86, 75, 66, 55, 97, 85, 97, 97, 95]; 
+let sortedScore = scores.sort();  
+
+// 숫자가 같으면 패스 
+sortedScore.forEach(element => {
+    let candidate = []; 
+    for(let i = 0; i < sortedScore.length; i++) {
+        if(element !== sortedScore[i]) {
+            candidate.push(element); 
+            console.log(candidate); 
+        }
+    }
+    console.log(candidate.length); 
+});
